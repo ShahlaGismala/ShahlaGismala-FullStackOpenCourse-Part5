3c57d0fd0da6445695fc5de5135dc0dfe4d61cb4
Student <student@sirafrica.org>
2026-07-15 11:29:47 +0300

    5.15

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
+import blogService from '../services/blogs'
 import Blog from './Blog'
+
+vi.mock('../services/blogs')
 
 describe('<Blog />', () => {
   const blog = {
+    id: '123',
     title: 'Component testing is done with react-testing-library',
     author: 'Kent C. Dodds',
     url: 'https://reactjs.org',
@@ -29,13 +34,29 @@ describe('<Blog />', () => {
   })
 
   test('url and likes are shown when the details button is clicked', async () => {
-  render(<Blog blog={blog} />)
+    render(<Blog blog={blog} />)
 
-  const user = userEvent.setup()
-  const button = screen.getByText('view')
-  await user.click(button)
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
-  screen.getByText('https://reactjs.org', { exact: false })
-  screen.getByText('likes', { exact: false })
-})
+    screen.getByText('https://reactjs.org', { exact: false })
+    screen.getByText('likes', { exact: false })
+  })
+
+  test('clicking the like button twice calls event handler twice', async () => {
+    blogService.update.mockResolvedValue({ ...blog, likes: blog.likes + 1 })
+
+    const mockHandler = vi.fn()
+    render(<Blog blog={blog} onUpdate={mockHandler} />)
+
+    const user = userEvent.setup()
+    await user.click(screen.getByText('view'))
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler).toHaveBeenCalledTimes(2)
+  })
 })
