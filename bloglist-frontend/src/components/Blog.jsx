c6f570b3c1c23cefa7db21c57952f8bd89a5f69b
Student <student@sirafrica.org>
2026-07-15 10:37:21 +0300

    5.6 BlogForm already has its own state. 5.7 toggle blog details

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setDetailsVisible(!detailsVisible)}>
+          {detailsVisible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {detailsVisible && (
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user?.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
